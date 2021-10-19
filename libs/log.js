25d4d7a73787cb98e7e3b6ebb195774a8359b82e
Misha <mglsh@ukr.net>
2021-10-20 00:29:20 +0300

    V5

--- libs/log.js
+++ libs/log.js
@@ -3,7 +3,7 @@ var winston = require('winston');
 function getLogger(module) {
     var path = module.filename.split('/').slice(-2).join('/'); //отобразим метку с именем файла, который выводит сообщение
 
-    return new winston.Logger({
+    return winston.createLogger({
         transports : [
             new winston.transports.Console({
                 colorize:   true,
