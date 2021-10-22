2224971d6ceacf5a1d4e1b92312e28e1806f004e
Misha <mglsh@ukr.net>
2021-10-23 00:34:00 +0300

    V6 Updata new site

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-var express         = require('express');
-var path            = require('path'); // модуль для парсинга пути
-var log             = require('./libs/log.js')(module);
+var express = require('express');
 var app = express();
+var fs = require("fs");
 
-app.use(express.favicon()); // отдаем стандартную фавиконку, можем здесь же свою задать
-app.use(express.logger('dev')); // выводим все запросы со статусами в консоль
-app.use(express.bodyParser()); // стандартный модуль, для парсинга JSON в запросах
-app.use(express.methodOverride()); // поддержка put и delete
-app.use(app.router); // модуль для простого задания обработчиков путей
-app.use(express.static(path.join(__dirname, "public"))); // запуск статического файлового сервера, который смотрит на папку public/ (в нашем случае отдает index.html)
+app.get('/listUsers', function (req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        console.log( data );
+        res.end( data );
+    });
+})
 
-app.get('/api', function (req, res) {
-    res.send('API is running');
-});
-
-app.listen(1337, function(){
-    log.info('Express server listening on port 1337');
-});
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://%s:%s", host, port)
+})
